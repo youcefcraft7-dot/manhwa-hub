221cd4be4b5a69614d0bb9c0f884447c878cd5a7
youcefcraft7-dot <youcefcraft7@gmail.com>
2026-05-22 23:26:33 +0100

    Implement manhwa retrieval functions

--- controllers/manhwasController.js
+++ controllers/manhwasController.js
@@ -1,1 +1,39 @@
+const manhwas = require("../data/manhwas");
 
+function getAllManhwas(req, res) {
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  });
+
+  res.end(JSON.stringify(manhwas));
+}
+
+function getManhwaById(req, res, id) {
+
+  const manhwa = manhwas.find(
+    item => item.id === id
+  );
+
+  if (!manhwa) {
+    res.writeHead(404, {
+      "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+      error: "Manhwa not found"
+    }));
+
+    return;
+  }
+
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  });
+
+  res.end(JSON.stringify(manhwa));
+}
+
+module.exports = {
+  getAllManhwas,
+  getManhwaById
+};
