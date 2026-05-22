62f12a109739c43aafd6a288e7bd375ef3ed907b
youcefcraft7-dot <youcefcraft7@gmail.com>
2026-05-22 19:53:32 +0100

    Add manhwas data with titles and chapters

---
+++ data/manhwas.js
@@ -0,0 +1,29 @@
+const manhwas = [
+  {
+    id: 1,
+    title: "Solo Leveling",
+    cover: "https://i.imgur.com/8Km9tLL.jpg",
+    chapters: [
+      { id: 1, title: "Chapter 1" },
+      { id: 2, title: "Chapter 2" }
+    ]
+  },
+  {
+    id: 2,
+    title: "The Beginning After The End",
+    cover: "https://i.imgur.com/7r6XK7F.jpg",
+    chapters: [
+      { id: 1, title: "Chapter 1" }
+    ]
+  },
+  {
+    id: 3,
+    title: "Omniscient Reader",
+    cover: "https://i.imgur.com/9J8LQ1R.jpg",
+    chapters: [
+      { id: 1, title: "Chapter 1" }
+    ]
+  }
+];
+
+module.exports = manhwas;
