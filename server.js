af842b172d3de194c46d605a7eba35e11407e835
youcefcraft7-dot <youcefcraft7@gmail.com>
2026-05-22 19:32:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,19 @@ const manhwas = [
   {
     id: 1,
     title: "Solo Leveling",
-    cover: "https://example.com/solo.jpg"
+    cover: "https://i.imgur.com/8Km9tLL.jpg"
   },
   {
     id: 2,
     title: "The Beginning After The End",
-    cover: "https://example.com/tbate.jpg"
+    cover: "https://i.imgur.com/7r6XK7F.jpg"
+  },
+  {
+    id: 3,
+    title: "Omniscient Reader",
+    cover: "https://i.imgur.com/9J8LQ1R.jpg"
   }
 ];
-
-const server = http.createServer((req, res) => {
-
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Manhwa Hub Backend is running!");
-  }
-
-  else if (req.url === "/api/manhwas") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(manhwas));
-  }
 
   else {
     res.writeHead(404);
